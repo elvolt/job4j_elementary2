d68dbb724ee46bb33c1b80e7d927126ea714ea83
elvolt <sidelevcs@gmail.com>
2020-12-08 20:50:19 +0300

    2.1. Рефакторинг - Расстояние между точками. [#312643]

--- src/main/java/ru/job4j/condition/Point.java
+++ src/main/java/ru/job4j/condition/Point.java
@@ -1,17 +1,25 @@
 package ru.job4j.condition;
 
-import java.text.DecimalFormat;
+import static java.lang.Math.sqrt;
+import static java.lang.Math.pow;
 
 public class Point {
-    public static double distance(int x1, int y1, int x2, int y2) {
-        return Math.sqrt((Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2)));
+    private int x;
+    private int y;
+
+    public Point(int x, int y) {
+        this.x = x;
+        this.y = y;
+    }
+
+    public double distance(Point that) {
+        return sqrt(pow(this.x - that.x, 2) + pow(this.y - that.y, 2));
     }
 
     public static void main(String[] args) {
-        DecimalFormat df = new DecimalFormat("0.00");
-        double result1 = Point.distance(0, 0, 2, 0);
-        double result2 = Point.distance(1, 1, 3, 0);
-        System.out.println("result (0, 0) to (2, 0) = " + df.format(result1));
-        System.out.println("result (1, 1) to (3, 0) = " + df.format(result2));
+        Point a = new Point(0, 0);
+        Point b = new Point(0, 2);
+        double dist = a.distance(b);
+        System.out.println(dist);
     }
 }
--- src/test/java/ru/job4j/condition/PointTest.java
+++ src/test/java/ru/job4j/condition/PointTest.java
@@ -8,12 +8,14 @@ public class PointTest {
 
     @Test
     public void when00To20Then2() {
+        Point point1 = new Point(0, 0);
+        Point point2 = new Point(2, 0);
         int x1 = 0;
         int y1 = 0;
         int x2 = 2;
         int y2 = 0;
         double expected = 2.0;
-        double result = Point.distance(x1, y1, x2, y2);
+        double result = point1.distance(point2);
         assertEquals(expected, result, 0.01);
     }
 }
